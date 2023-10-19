50ebebea35ef43aabf69438932fcad6907bee11f
abdusemed musema <abdumussema22@gmail.com>
2023-10-19 10:00:26 +0300

    routes

---
+++ lib/Pages/routes/home3.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Home3 extends StatefulWidget {
+  const Home3({super.key});
+
+  @override
+  State<Home3> createState() => _Home3State();
+}
+
+class _Home3State extends State<Home3> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text("Home"),
+      ),
+    );
+  }
+}
