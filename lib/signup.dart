78f8fd9fdbd09af785015839f750a3c92a5a5c13
abdusemed musema <abdumussema22@gmail.com>
2023-10-30 15:37:29 +0300

    Signup API integrated

--- lib/signup.dart
+++ lib/signup.dart
@@ -1,6 +1,10 @@
+import 'dart:convert';
 import 'package:dropdown_textfield/dropdown_textfield.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:vsla/otp.dart';
+import 'package:http/http.dart' as http;
 
 class Signup extends StatefulWidget {
   const Signup({super.key});
@@ -9,20 +13,128 @@ class Signup extends StatefulWidget {
   State<Signup> createState() => _SignupState();
 }
 
+class CompanyData {
+  final int companyId;
+  final String companyName;
+
+  CompanyData({
+    required this.companyId,
+    required this.companyName,
+  });
+}
+
 class _SignupState extends State<Signup> {
   // FocusNode phoneFocus = FocusNode();
   late SingleValueDropDownController _cnt;
+  List<CompanyData> company = [];
+  String? selectedCompany;
+
+// Function to handle dropdown value changes
+  void onChanged(String? value) {
+    print(value);
+    setState(() {
+      selectedCompany = value;
+    });
+  }
 
   @override
   void initState() {
     _cnt = SingleValueDropDownController();
     super.initState();
+    fetchCompany();
   }
 
   @override
   void dispose() {
     _cnt.dispose();
     super.dispose();
+  }
+
+  TextEditingController fname = TextEditingController();
+  TextEditingController pnumber = TextEditingController();
+  TextEditingController password = TextEditingController();
+  TextEditingController cpassword = TextEditingController();
+  bool _passwordVisible1 = false;
+  bool _passwordVisible = false;
+  bool loading = false;
+  signup() async {
+    // print(pnumber);
+    if (fname.text.isEmpty) {
+      const message = 'Full name is mandatory';
+      Future.delayed(const Duration(milliseconds: 100), () {
+        Fluttertoast.showToast(msg: message, fontSize: 18);
+      });
+    } else if (pnumber.text.length != 10 || pnumber.text == "") {
+      const message = 'Invalid phone number format';
+      Future.delayed(const Duration(milliseconds: 100), () {
+        Fluttertoast.showToast(msg: message, fontSize: 18);
+      });
+    } else if (password.text != cpassword.text || password.text == "") {
+      const message = 'Invalid password';
+      Future.delayed(const Duration(milliseconds: 100), () {
+        Fluttertoast.showToast(msg: message, fontSize: 18);
+      });
+    } else {
+      setState(() {
+        loading = true;
+      });
+      final body = {
+        "phoneNumber": pnumber.text,
+        "password": password.text,
+        "fullName": fname.text,
+        "roleName": "GROUP_ADMIN",
+        "companyId": selectedCompany
+      };
+      print(body);
+      try {
+        var response = await http.post(
+          Uri.http("10.1.177.121:8111", "api/v1/users"),
+          headers: <String, String>{
+            'Content-Type': 'application/json; charset=UTF-8',
+          },
+          body: jsonEncode(body),
+        );
+        print("here" + "${response.statusCode}");
+        print(response.body);
+        if (response.statusCode == 201) {
+          setState(() {
+            loading = false;
+          });
+          const message = 'Account Created Successfuly!';
+          Future.delayed(const Duration(milliseconds: 100), () {
+            Fluttertoast.showToast(msg: message, fontSize: 18);
+          });
+
+          // ignore: use_build_context_synchronously
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => const Otp()));
+          setState(() {
+            loading = false;
+          });
+        } else if (response.statusCode != 201) {
+          final responseBody = json.decode(response.body);
+          final description =
+              responseBody['message']; // Extract 'description' field
+          if (description == "user already exists") {
+            Fluttertoast.showToast(
+                msg: "This phone number is already registered", fontSize: 18);
+          } else {
+            const message = 'Account Creation Faild! Try again';
+            Fluttertoast.showToast(msg: message, fontSize: 18);
+          }
+          setState(() {
+            loading = false;
+          });
+        }
+      } catch (e) {
+        const message = 'Please check your network connection';
+        Fluttertoast.showToast(msg: message, fontSize: 18);
+      } finally {
+        setState(() {
+          loading = false;
+        });
+      }
+    }
   }
 
   @override
@@ -83,11 +195,12 @@ class _SignupState extends State<Signup> {
                     child: Padding(
                       padding: const EdgeInsets.only(left: 8.0),
                       child: TextField(
+                        controller: fname,
                         // focusNode: phoneFocus,
                         // onTapOutside: (event) {
                         //   phoneFocus.unfocus();
                         // },
-                        keyboardType: TextInputType.phone,
+                        keyboardType: TextInputType.text,
                         decoration: InputDecoration(
                           border: InputBorder.none,
                           // prefixIcon: Icon(
@@ -126,6 +239,7 @@ class _SignupState extends State<Signup> {
                     child: Padding(
                       padding: const EdgeInsets.only(left: 8.0),
                       child: TextField(
+                        controller: pnumber,
                         // focusNode: phoneFocus,
                         // onTapOutside: (event) {
                         //   phoneFocus.unfocus();
@@ -140,10 +254,10 @@ class _SignupState extends State<Signup> {
                           hintStyle:
                               GoogleFonts.poppins(color: Colors.grey[400]),
                         ),
-                        onChanged: (value) {
-                          // Handle the phone number input here
-                          print('Phone Number: $value');
-                        },
+                        // onChanged: (value) {
+                        //   // Handle the phone number input here
+                        //   print('Phone Number: $value');
+                        // },
                       ),
                     ),
                   ),
@@ -169,23 +283,40 @@ class _SignupState extends State<Signup> {
                       child: Padding(
                         padding: const EdgeInsets.only(left: 8.0),
                         child: TextField(
+                          controller: password,
                           obscuringCharacter: "*",
                           // focusNode: pinFocus,
                           // onTapOutside: (event) {
                           //   pinFocus.unfocus();
                           // },
-                          obscureText: true,
+                          obscureText: !_passwordVisible,
                           keyboardType: TextInputType.phone,
                           decoration: InputDecoration(
                             border: InputBorder.none,
                             hintText: "******",
                             hintStyle:
                                 GoogleFonts.poppins(color: Colors.grey[400]),
+                            suffixIcon: IconButton(
+                              // tooltip: "show password",
+                              icon: Icon(
+                                // Based on passwordVisible state choose the icon
+                                _passwordVisible
+                                    ? Icons.visibility
+                                    : Icons.visibility_off,
+                                color: Theme.of(context).primaryColorDark,
+                              ),
+                              onPressed: () {
+                                // Update the state i.e. toogle the state of passwordVisible variable
+                                setState(() {
+                                  _passwordVisible = !_passwordVisible;
+                                });
+                              },
+                            ),
                           ),
-                          onChanged: (value) {
-                            // Handle the phone number input here
-                            print('Phone Number: $value');
-                          },
+                          // onChanged: (value) {
+                          //   // Handle the phone number input here
+                          //   print('Phone Number: $value');
+                          // },
                         ),
                       ),
                     ),
@@ -212,18 +343,34 @@ class _SignupState extends State<Signup> {
                         child: Padding(
                           padding: const EdgeInsets.all(8.0),
                           child: TextField(
+                            controller: cpassword,
                             obscuringCharacter: "*",
                             // focusNode: pinFocus,
                             // onTapOutside: (event) {
                             //   pinFocus.unfocus();
                             // },
-                            obscureText: true,
+                            obscureText: !_passwordVisible1,
                             keyboardType: TextInputType.phone,
                             decoration: InputDecoration(
                               border: InputBorder.none,
                               hintText: "******",
                               hintStyle:
                                   GoogleFonts.poppins(color: Colors.grey[400]),
+                              suffixIcon: IconButton(
+                                icon: Icon(
+                                  // Based on passwordVisible state choose the icon
+                                  _passwordVisible1
+                                      ? Icons.visibility
+                                      : Icons.visibility_off,
+                                  color: Theme.of(context).primaryColorDark,
+                                ),
+                                onPressed: () {
+                                  // Update the state i.e. toogle the state of passwordVisible variable
+                                  setState(() {
+                                    _passwordVisible1 = !_passwordVisible1;
+                                  });
+                                },
+                              ),
                             ),
                             onChanged: (value) {
                               // Handle the phone number input here
@@ -252,48 +399,51 @@ class _SignupState extends State<Signup> {
                         borderRadius: BorderRadius.circular(10),
                       ),
                       child: Padding(
-                        padding: const EdgeInsets.only(left: 8.0),
-                        child: DropDownTextField(
-                          // initialValue: "name4",
-                          // readOnly: false,
-                          controller: _cnt,
-                          clearOption: true,
-                          enableSearch: true,
-                          keyboardType: TextInputType.number,
-                          autovalidateMode: AutovalidateMode.always,
-                          clearIconProperty: IconProperty(color: Colors.green),
-                          searchDecoration:
-                              const InputDecoration(hintText: "Serach"),
-                          // validator: (value) {
-                          //   if (value == null || value.isEmpty) {
-                          //     return "Required field";
-                          //   } else {
-                          //     return null;
-                          //   }
-                          // },
-                          dropDownItemCount: 6,
+                          padding: const EdgeInsets.only(left: 0),
+                          child: DropdownButtonFormField<String>(
+                            decoration: InputDecoration(
+                              contentPadding: const EdgeInsets.fromLTRB(
+                                  12, 10.0, 12.0, 10.0),
+                              // labelText: "select company",
+                              // hintText: "Choose zone/subcity",
+                              // labelStyle: GoogleFonts.poppins(
+                              //     fontSize: 14, color: Color(0xFFF89520)),
+                              // hintStyle: GoogleFonts.poppins(
+                              //     fontSize: 14, color: Color(0xFFF89520)),
+                              enabledBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10.0),
+                                borderSide:
+                                    const BorderSide(color: Color(0xFFF89520)),
+                              ),
+                              focusedBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10.0),
+                                borderSide:
+                                    const BorderSide(color: Color(0xFFF89520)),
+                              ),
+                              focusedErrorBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10.0),
+                                borderSide:
+                                    const BorderSide(color: Color(0xFFF89520)),
+                              ),
+                              filled: true,
+                              fillColor: Colors.transparent,
+                            ),
+                            value:
+                                selectedCompany, // Initially selected value (can be null)
+                            onChanged:
+                                onChanged, // Function to handle value changes
 
-                          dropDownList: const [
-                            DropDownValueModel(name: 'name1', value: "value1"),
-                            DropDownValueModel(
-                                name: 'name2',
-                                value: "value2",
-                                toolTipMsg:
-                                    "DropDownButton is a widget that we can use to select one unique value from a set of values"),
-                            DropDownValueModel(name: 'name3', value: "value3"),
-                            DropDownValueModel(
-                                name: 'name4',
-                                value: "value4",
-                                toolTipMsg:
-                                    "DropDownButton is a widget that we can use to select one unique value from a set of values"),
-                            DropDownValueModel(name: 'name5', value: "value5"),
-                            DropDownValueModel(name: 'name6', value: "value6"),
-                            DropDownValueModel(name: 'name7', value: "value7"),
-                            DropDownValueModel(name: 'name8', value: "value8"),
-                          ],
-                          onChanged: (val) {},
-                        ),
-                      ),
+                            items: company.map((CompanyData companies) {
+                              return DropdownMenuItem<String>(
+                                value: companies.companyId.toString(),
+                                child: Text(
+                                  companies.companyName,
+                                  style: TextStyle(
+                                      fontSize: 14, color: Colors.black),
+                                ),
+                              );
+                            }).toList(),
+                          )),
                     ),
                   ]),
             ),
@@ -315,35 +465,40 @@ class _SignupState extends State<Signup> {
                 ),
               ],
             ),
-            Padding(
-              padding: EdgeInsets.symmetric(
-                horizontal: MediaQuery.of(context).size.width * 0.2,
-                vertical: MediaQuery.of(context).size.height * 0.015,
-              ),
-              child: GestureDetector(
-                onTap: () {
-                  // login();
-                },
-                child: Container(
-                  padding:
-                      EdgeInsets.all(MediaQuery.of(context).size.height * 0.01),
-                  decoration: BoxDecoration(
-                    color: Colors.orange, // You can use your color here
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: Center(
-                    child: Text(
-                      "SING UP",
-                      style: GoogleFonts.playfairDisplay(
-                        color: Colors.white, // You can use your color here
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
+            loading
+                ? const CircularProgressIndicator(
+                    color: Colors.orange,
+                  )
+                : Padding(
+                    padding: EdgeInsets.symmetric(
+                      horizontal: MediaQuery.of(context).size.width * 0.2,
+                      vertical: MediaQuery.of(context).size.height * 0.015,
+                    ),
+                    child: GestureDetector(
+                      onTap: () {
+                        signup();
+                      },
+                      child: Container(
+                        padding: EdgeInsets.all(
+                            MediaQuery.of(context).size.height * 0.01),
+                        decoration: BoxDecoration(
+                          color: Colors.orange, // You can use your color here
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Center(
+                          child: Text(
+                            "SING UP",
+                            style: GoogleFonts.playfairDisplay(
+                              color:
+                                  Colors.white, // You can use your color here
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
                       ),
                     ),
                   ),
-                ),
-              ),
-            ),
             Center(
               child: Text(
                 "or sign in with",
@@ -420,5 +575,46 @@ class _SignupState extends State<Signup> {
         ),
       )),
     );
+  }
+
+  Future<void> fetchCompany() async {
+    try {
+      // var user = await SimplePreferences().getUser();
+
+      final response = await http.get(
+        Uri.http('10.1.177.121:8111', '/api/v1/companies'),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+      );
+      // transactions = parseTransactions(response.body);
+      var data = jsonDecode(response.body);
+
+      print(data);
+      List<CompanyData> newCompany = [];
+
+      for (var comapany in data) {
+        // print(transaction.date);
+        var companyData = CompanyData(
+          companyId: comapany['companyId'],
+          companyName: comapany['companyName'],
+        );
+        newCompany.add(companyData);
+        print(company);
+      }
+      company.addAll(newCompany);
+      print(company.length);
+
+      // print(transactions[0]);
+
+      // setState(() {
+      //   loading = false;
+      // }
+      // );
+    } catch (e) {
+      var message = e.toString();
+      'Something went wrong. Please check your internet connection.';
+      Fluttertoast.showToast(msg: message, fontSize: 18);
+    }
   }
 }
