c9f3f1617a91a30c793e7d1eeba559f3d6385769
abdusemed musema <abdumussema22@gmail.com>
2023-11-17 15:13:40 +0300

    Adjust

--- lib/Pages/routes/settings.dart
+++ lib/Pages/routes/settings.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:vsla/login.dart';
 
 class Settings extends StatefulWidget {
   const Settings({super.key});
@@ -10,10 +12,62 @@ class Settings extends StatefulWidget {
 class _SettingsState extends State<Settings> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text("settings"),
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                GestureDetector(
+                    onTap: () {
+                      _onBackButtonPressed(context);
+                    },
+                    child: const Icon(Icons.arrow_back_ios_new_sharp)),
+                Image(
+                    height: MediaQuery.of(context).size.height * 0.05,
+                    image: const AssetImage("assets/images/vsla.png"))
+              ],
+            ),
+          ),
+        ],
       ),
     );
+  }
+
+  Future<bool> _onBackButtonPressed(BuildContext context) async {
+    return await showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            backgroundColor: Colors.white,
+            title: const Text("Confirm Exit"),
+            content: const Text("Do you want to Logout?"),
+            actions: <Widget>[
+              TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop(false);
+                  },
+                  child: const Text("No")),
+              TextButton(
+                  onPressed: () async {
+                    List<String> user = [];
+                    final SharedPreferences prefs =
+                        await SharedPreferences.getInstance();
+
+                    prefs.setStringList("_keyUser", user);
+                    // ignore: use_build_context_synchronously
+                    Navigator.pushReplacement(context,
+                        MaterialPageRoute(builder: (context) => const Login()));
+                  },
+                  child: const Text(
+                    "Yes",
+                    style: TextStyle(color: Colors.red),
+                  ))
+            ],
+          );
+        });
   }
 }
